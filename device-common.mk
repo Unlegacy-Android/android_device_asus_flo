edb71b05eab5e50495cdb26eaec39965084aae91
Svyatoslav Ryhel <clamor95@gmail.com>
2018-01-13 13:31:53 +0200

    Reorganize device-common makefile
    Change-Id: I3659aaaea0ac88f1e4c998c566b0bddbfe1aefad
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -19,25 +19,30 @@
 #
 # Everything in this directory will become public
 
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+PRODUCT_COPY_FILES += \
+	device/asus/flo/rootdir/init.flo.usb.rc:root/init.flo.usb.rc \
+	device/asus/flo/rootdir/ueventd.flo.rc:root/ueventd.flo.rc
+
+# Wi-Fi & Bluetooth
 PRODUCT_PACKAGES := \
-    libwpa_client \
-    hostapd \
-    dhcpcd.conf \
-    wpa_supplicant \
-    wpa_supplicant.conf
+	libwpa_client \
+	hostapd \
+	dhcpcd.conf \
+	wpa_supplicant \
+	wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-	lights.msm8960
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-        LiveWallpapersPicker \
-        librs_jni
+	bdAddrLoader \
+	libwfcu \
+	conn_init
 
 PRODUCT_COPY_FILES += \
 	device/asus/flo/wifi/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
@@ -47,26 +52,30 @@ PRODUCT_COPY_FILES += \
 	device/asus/flo/wifi/init.flo.wifi.sh:system/bin/init.flo.wifi.sh
 
 PRODUCT_COPY_FILES += \
-	device/asus/flo/audio/audio_policy.conf:system/etc/audio_policy.conf
+	device/asus/flo/bluetooth/init.flo.bt.sh:system/bin/init.flo.bt.sh
 
-PRODUCT_COPY_FILES += \
-	device/asus/flo/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-	device/asus/flo/configs/sec_config:system/etc/sec_config
+# Lights
+PRODUCT_PACKAGES += \
+	lights.msm8960
+
+PRODUCT_PACKAGES += \
+	charger_res_images
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+	LiveWallpapersPicker \
+	librs_jni
 
 PRODUCT_COPY_FILES += \
 	device/asus/flo/configs/thermald-flo.conf:system/etc/thermald.conf
 
+# Media
 PRODUCT_COPY_FILES += \
-	device/asus/flo/rootdir/init.flo.usb.rc:root/init.flo.usb.rc \
-	device/asus/flo/rootdir/ueventd.flo.rc:root/ueventd.flo.rc \
-	device/asus/flo/media/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+	device/asus/flo/media/media_profiles.xml:system/etc/media_profiles.xml \
 	device/asus/flo/media/media_codecs.xml:system/etc/media_codecs.xml \
 	device/asus/flo/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
-
-PRODUCT_COPY_FILES += \
-	device/asus/flo/kickstart_checker.sh:system/bin/kickstart_checker.sh
 
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
@@ -81,10 +90,7 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilt input device calibration files
 PRODUCT_COPY_FILES += \
-	device/asus/flo/configs/touch_dev.idc:system/usr/idc/touch_dev.idc
-
-# TP idc file
-PRODUCT_COPY_FILES += \
+	device/asus/flo/configs/touch_dev.idc:system/usr/idc/touch_dev.idc \
 	device/asus/flo/configs/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
 
 # These are the hardware-specific features
@@ -111,7 +117,8 @@ PRODUCT_PACKAGES += \
 	ks \
 	efsks
 
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
+PRODUCT_COPY_FILES += \
+	device/asus/flo/kickstart_checker.sh:system/bin/kickstart_checker.sh
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
@@ -126,6 +133,7 @@ PRODUCT_PACKAGES += \
 	keystore.msm8960 \
 	memtrack.msm8960
 
+# Audio
 PRODUCT_PACKAGES += \
 	audio.primary.msm8960 \
 	audio.a2dp.default \
@@ -133,18 +141,19 @@ PRODUCT_PACKAGES += \
 	audio.r_submix.default \
 	libaudio-resampler
 
+PRODUCT_COPY_FILES += \
+	device/asus/flo/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+	device/asus/flo/audio/audio_policy.conf:system/etc/audio_policy.conf
+
 # Voice processing
 PRODUCT_PACKAGES += \
-    libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors
+	libqcomvoiceprocessing \
+	libqcomvoiceprocessingdescriptors
 
 PRODUCT_COPY_FILES += \
-    device/asus/flo/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+	device/asus/flo/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
-PRODUCT_COPY_FILES += \
-	device/asus/flo/bluetooth/init.flo.bt.sh:system/bin/init.flo.bt.sh
-
-
+# Camera
 PRODUCT_PACKAGES += \
 	libmmcamera_interface2 \
 	libmmcamera_interface
@@ -159,35 +168,29 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libloc_eng \
-    libloc_core \
-    libgps.utils \
-    gps.conf \
-    gps.msm8960
+	libloc_eng \
+	libloc_core \
+	libgps.utils \
+	gps.conf \
+	gps.msm8960
 
+PRODUCT_COPY_FILES += \
+	device/asus/flo/configs/sec_config:system/etc/sec_config
+
+# Power HAL
 PRODUCT_PACKAGES += \
-	bdAddrLoader \
-	libwfcu \
-	conn_init
-
-PRODUCT_PACKAGES += \
-	wpa_supplicant_overlay.conf \
-	p2p_supplicant_overlay.conf
-
-
-PRODUCT_PACKAGES += \
-    power.flo
+	power.flo
 
 # NFC packages
 PRODUCT_PACKAGES += \
-    NfcNci \
-    Tag
+	NfcNci \
+	Tag
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    device/asus/flo/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/asus/flo/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+	device/asus/flo/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+	device/asus/flo/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
