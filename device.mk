52c578a9f4f080c255918a3d188aa085abb55c1e
Svyatoslav Ryhel <clamor95@gmail.com>
2018-01-15 19:14:39 +0200

    Unite device.mk and device-common.mk
    Change-Id: I8645782671159281c9045335286a57859804833c
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device.mk
+++ device.mk
@@ -19,24 +19,187 @@
 #
 # Everything in this directory will become public
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.carrier=wifi-only
-
-PRODUCT_COPY_FILES := \
-    device/asus/flo/rootdir/fstab.flo:root/fstab.flo \
-    device/asus/flo/rootdir/init.flo.rc:root/init.flo.rc
-
-PRODUCT_PACKAGES += \
-    camera.flo
-
-# NFC
-PRODUCT_PACKAGES += \
-    nfc_nci.bcm2079x.default
-
-# the actual meat of the device-specific product definition
-$(call inherit-product, device/asus/flo/device-common.mk)
-
-# inherit from the non-open-source side, if present
-$(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
-
 DEVICE_PACKAGE_OVERLAYS := device/asus/flo/overlay
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Rootdir
+PRODUCT_COPY_FILES += \
+	device/asus/flo/rootdir/fstab.flo:root/fstab.flo \
+	device/asus/flo/rootdir/init.flo.rc:root/init.flo.rc \
+	device/asus/flo/rootdir/init.flo.usb.rc:root/init.flo.usb.rc \
+	device/asus/flo/rootdir/ueventd.flo.rc:root/ueventd.flo.rc
+
+# Wi-Fi & Bluetooth
+PRODUCT_PACKAGES := \
+	libwpa_client \
+	hostapd \
+	dhcpcd.conf \
+	wpa_supplicant \
+	wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
+
+PRODUCT_PACKAGES += \
+	bdAddrLoader \
+	libwfcu \
+	conn_init
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/wifi/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
+	device/asus/flo/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+	device/asus/flo/wifi/WCNSS_qcom_wlan_nv_flo.bin:system/etc/wifi/WCNSS_qcom_wlan_nv_flo.bin \
+	device/asus/flo/wifi/WCNSS_qcom_wlan_nv_deb.bin:system/etc/wifi/WCNSS_qcom_wlan_nv_deb.bin \
+	device/asus/flo/wifi/init.flo.wifi.sh:system/bin/init.flo.wifi.sh
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/bluetooth/init.flo.bt.sh:system/bin/init.flo.bt.sh
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.msm8960
+
+PRODUCT_PACKAGES += \
+	charger_res_images
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+	LiveWallpapersPicker \
+	librs_jni
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/configs/thermald-flo.conf:system/etc/thermald.conf
+
+# Media
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+	device/asus/flo/media/media_profiles.xml:system/etc/media_profiles.xml \
+	device/asus/flo/media/media_codecs.xml:system/etc/media_codecs.xml \
+	device/asus/flo/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+
+# Prebuilt kl and kcm keymaps
+PRODUCT_COPY_FILES += \
+	device/asus/flo/keylayout/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
+	device/asus/flo/keylayout/hs_detect.kl:system/usr/keylayout/hs_detect.kl \
+	device/asus/flo/keylayout/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
+	device/asus/flo/keylayout/keypad_8064.kl:system/usr/keylayout/keypad_8064.kl \
+	device/asus/flo/keychars/apq8064-tabla-snd-card_Button_Jack.kcm:system/usr/keychars/apq8064-tabla-snd-card_Button_Jack.kcm \
+	device/asus/flo/keychars/hs_detect.kcm:system/usr/keychars/hs_detect.kcm \
+	device/asus/flo/keychars/keypad_8064.kcm:system/usr/keychars/keypad_8064.kcm \
+	device/asus/flo/keychars/pmic8xxx_pwrkey.kcm:system/usr/keychars/pmic8xxx_pwrkey.kcm
+
+# Prebuilt input device calibration files
+PRODUCT_COPY_FILES += \
+	device/asus/flo/configs/touch_dev.idc:system/usr/idc/touch_dev.idc \
+	device/asus/flo/configs/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+
+# Kickstart
+PRODUCT_PACKAGES += \
+	qcks \
+	ks \
+	efsks
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/kickstart_checker.sh:system/bin/kickstart_checker.sh
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	e2fsck
+
+PRODUCT_PACKAGES += \
+	libgenlock \
+	liboverlay \
+	hwcomposer.msm8960 \
+	gralloc.msm8960 \
+	copybit.msm8960 \
+	keystore.msm8960 \
+	memtrack.msm8960
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.primary.msm8960 \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	libaudio-resampler
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+	device/asus/flo/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Voice processing
+PRODUCT_PACKAGES += \
+	libqcomvoiceprocessing \
+	libqcomvoiceprocessingdescriptors
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+# Camera
+PRODUCT_PACKAGES += \
+	camera.flo \
+	libmmcamera_interface2 \
+	libmmcamera_interface
+
+PRODUCT_PACKAGES += \
+	libdivxdrmdecrypt \
+	libOmxVdec \
+	libOmxVenc \
+	libOmxCore \
+	libstagefrighthw \
+	libc2dcolorconvert
+
+# GPS
+PRODUCT_PACKAGES += \
+	libloc_eng \
+	libloc_core \
+	libgps.utils \
+	gps.conf \
+	gps.msm8960
+
+PRODUCT_COPY_FILES += \
+	device/asus/flo/configs/sec_config:system/etc/sec_config
+
+# Power HAL
+PRODUCT_PACKAGES += \
+	power.flo
+
+# NFC packages
+PRODUCT_PACKAGES += \
+	nfc_nci.bcm2079x.default \
+	NfcNci \
+	Tag
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+	device/asus/flo/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+	device/asus/flo/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf
+
+$(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+$(call inherit-product-if-exists, vendor/widevine/arm-generic/widevine-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
