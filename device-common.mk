82b9cae33cdc6f1b357a57184d6251920caa3f3a
Daniel Jarai <jaraidaniel@gmail.com>
2018-02-16 20:31:56 +0200

    Enable treble OMX
    Created media_profiles_V1_0.xml from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: Id4398dc3259a7ce9facbcd6e97af63b1ad92dde5
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -88,7 +88,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-	device/asus/flo/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+	device/asus/flo/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
 	device/asus/flo/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
 	device/asus/flo/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
